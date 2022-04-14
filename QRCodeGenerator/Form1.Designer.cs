613f3ab459eb3f4bd3cb5cc49f8d544218e89fb8
laser <laser@XIAGAXPC>
2022-04-13 21:58:05 -0500

    Add project files.

---
+++ QRCodeGenerator/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace QRCodeGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.QRCodeBox = new System.Windows.Forms.PictureBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.QRCodeBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnExit);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(563, 38);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MouseDown_Event);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MoveMove_Event);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.MouseUp_Event);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(205, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "QR Code Generator";
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MouseDown_Event);
+            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MoveMove_Event);
+            this.label1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.MouseUp_Event);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnExit.BackgroundImage = global::QRCodeGenerator.Properties.Resources.Close_48px;
+            this.btnExit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnExit.Location = new System.Drawing.Point(528, 6);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(27, 27);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // QRCodeBox
+            // 
+            this.QRCodeBox.Location = new System.Drawing.Point(74, 56);
+            this.QRCodeBox.Name = "QRCodeBox";
+            this.QRCodeBox.Size = new System.Drawing.Size(418, 258);
+            this.QRCodeBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.QRCodeBox.TabIndex = 1;
+            this.QRCodeBox.TabStop = false;
+            this.QRCodeBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MouseDown_Event);
+            this.QRCodeBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MoveMove_Event);
+            this.QRCodeBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.MouseUp_Event);
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.BackColor = System.Drawing.SystemColors.Window;
+            this.btnGenerate.FlatAppearance.BorderSize = 0;
+            this.btnGenerate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGenerate.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerate.Location = new System.Drawing.Point(74, 352);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(418, 38);
+            this.btnGenerate.TabIndex = 2;
+            this.btnGenerate.Text = "Generate QR Code";
+            this.btnGenerate.UseVisualStyleBackColor = false;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(74, 320);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(418, 29);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.MouseHover += new System.EventHandler(this.MouseHover_Event);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(563, 402);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.QRCodeBox);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MouseDown_Event);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MoveMove_Event);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.MouseUp_Event);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.QRCodeBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.PictureBox QRCodeBox;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ QRCodeGenerator/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+
+
+namespace QRCodeGenerator
+{
+
+    public partial class Form1 : Form
+    {
+
+        SoundPlayer soundPlayer = new SoundPlayer(@"C:\Users\laser\OneDrive\Desktop\Coding Projects\C#\QRCodeGenerator\MMZ3Selection.wav");
+        bool mouseDown;
+        Point lastPoint;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+       
+        //allow the form to be draggable
+        private void MouseDown_Event(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            lastPoint = new Point(e.X, e.Y);
+
+        }
+
+        private void MoveMove_Event(object sender, MouseEventArgs e)
+        {
+            if (mouseDown == true)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+
+            }
+        }
+
+        private void MouseUp_Event(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+        //add a tool tip to give directions to use
+        private void MouseHover_Event(object sender, EventArgs e)
+        {
+            toolTip1.Show("Type or Paste text into here and click Generate QR Code", textBox1);
+        }
+        //generate the QR code based on user input and play a sound to notify the conversion has started
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            soundPlayer.Play();
+            QRCoder.QRCodeGenerator qr = new QRCoder.QRCodeGenerator();
+            var MyData = qr.CreateQrCode(textBox1.Text, QRCoder.QRCodeGenerator.ECCLevel.H);
+            var code = new QRCoder.QRCode(MyData);
+            QRCodeBox.Image = code.GetGraphic(50);
+        }
+        //Add confirmation functionality to exit button
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Do you want to exit the program?", "Exit?", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                Application.Exit(); 
+            }
+            if(DialogResult == DialogResult.Cancel)
+                {
+                this.Close();
+            }
+        }
+    }
+}
